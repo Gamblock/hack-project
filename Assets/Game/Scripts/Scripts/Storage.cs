c919e54550f9135bf64abab2f2724f0aff8ea6aa
saany <saanyanice@gmail.com>
2021-10-24 17:44:49 +0300

    fix git

--- Assets/Game/Scripts/Scripts/Storage.cs
+++ Assets/Game/Scripts/Scripts/Storage.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Storage : ScriptableObject
 {
+    public void GetDataByKey(string key)
+    {
+        ES3.Load(key);
+    }
     public void SaveData<T>(string key,T value)
     {
         ES3.Save(key,value);
